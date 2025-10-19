8e69c26cea0fae61791ea8d7c5a9527164d93b47
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2025-10-19 13:40:41 +0800

    refactor: renovate

---
+++ src/components/IconItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <img
+    class="icon"
+    :alt="alt"
+    :src="src"
+    :width="size"
+    :height="size"
+    @click="onClickandGo(url)"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IconItem",
+  props: {
+    alt: {
+      type: String,
+      default: "icon",
+    },
+    src: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      default: "",
+    },
+    size: {
+      type: [Number, String],
+      default: 30,
+    },
+  },
+  setup() {
+    const onClickandGo = (url: string) => {
+      window.open(url, "_blank");
+    };
+
+    return {
+      onClickandGo,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.icon:hover {
+  cursor: pointer;
+  transform: scale(1.1);
+  transition: transform 0.1s;
+}
+</style>
